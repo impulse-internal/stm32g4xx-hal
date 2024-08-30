bcb1531005cf4f692e7bb93e88b5875f1aa28bb0
Jean-Baptiste Theou <jb@impulselabs.com>
2024-08-30 09:06:43 -1000

    mod: disable LSE in force_reset_backup
    Signed-off-by: Jean-Baptiste Theou <jb@impulselabs.com>

--- src/rcc/mod.rs
+++ src/rcc/mod.rs
@@ -496,6 +496,9 @@ impl Rcc {
     pub fn force_reset_backup(&self) {
         self.rb
             .bdcr
+            .modify(|_, w| w.lseon().clear_bit());
+        self.rb
+            .bdcr
             .modify(|_, w| w.bdrst().set_bit());
     }
 
